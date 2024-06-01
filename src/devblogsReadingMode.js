1154fe44c72fe62660bb9e6d2a69c78d78af3eea
Hakan Fıstık <hakam.fstk@gmail.com>
2024-06-01 12:28:06 +0200

    first commit

---
+++ src/devblogsReadingMode.js
@@ -0,0 +1,47 @@
+// remove social media button on the left
+var element = document.querySelector('div.social-icon-general');
+element.parentElement.removeChild(element);
+
+// remove header
+var element = document.querySelector('header.c-uhfh');
+element.parentElement.removeChild(element);
+
+// remove comments button on the side
+var element = document.querySelector('div.back-comment-buttons');
+element.parentElement.removeChild(element);
+
+// remove feedback button on the side
+var element = document.querySelector('div.vscom-usabilla-button');
+element.parentElement.removeChild(element);
+
+// remove feedback button on the side
+var element = document.querySelector('div.related-postssection');
+element.parentElement.removeChild(element);
+
+// remove comments area
+var element = document.querySelector('div.comments-area');
+element.parentElement.removeChild(element);
+
+// remove footer region
+var element = document.querySelector('#footerRegion');
+element.parentElement.removeChild(element);
+
+// remove footer region
+var element = document.querySelector('#wrapper-footer-1');
+element.parentElement.removeChild(element);
+
+// remove footer region
+var element = document.querySelector('footer.cattagsarea');
+element.parentElement.removeChild(element);
+
+// remove paddings and margins
+var element = document.querySelector('#single-wrapper');
+element.style.paddingTop = '0px';
+element.style.paddingBottom = '0px';
+
+var element = document.querySelector('#featured');
+element.style.paddingLeft = '0px';
+element.style.paddingRight = '0px';
+element.style.marginRight = '0px';
+element.style.marginLeft = '0px';
+element.style.paddingTop = '0px';
