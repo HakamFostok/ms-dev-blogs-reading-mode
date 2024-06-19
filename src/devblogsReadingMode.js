0bd9966ba39d89980cfa700e00743fe2324005b0
Hakan Fıstık <hakam.fstk@gmail.com>
2024-06-19 17:37:50 +0200

    refactoring

--- src/devblogsReadingMode.js
+++ src/devblogsReadingMode.js
@@ -1,71 +1,68 @@
 // remove social media button on the left
-var element = document.querySelector('div.social-icon-general');
-element.parentElement.removeChild(element);
+removeElement('div.social-icon-general');
 
 // remove header
-var element = document.querySelector('header.c-uhfh');
-element.parentElement.removeChild(element);
+removeElement('header.c-uhfh');
 
 // remove comments button on the side
-var element = document.querySelector('div.back-comment-buttons');
-element.parentElement.removeChild(element);
+removeElement('div.back-comment-buttons');
 
 // remove feedback button on the side
-var element = document.querySelector('div.vscom-usabilla-button');
-element.parentElement.removeChild(element);
+removeElement('div.vscom-usabilla-button');
 
 // remove feedback button on the side
-var element = document.querySelector('div.related-postssection');
-element.parentElement.removeChild(element);
+removeElement('div.related-postssection');
 
 // remove comments area
-var element = document.querySelector('div.comments-area');
-element.parentElement.removeChild(element);
+removeElement('div.comments-area');
 
 // remove footer region
-var element = document.querySelector('#footerRegion');
-element.parentElement.removeChild(element);
+removeElement('#footerRegion');
 
 // remove footer region
-var element = document.querySelector('#wrapper-footer-1');
-element.parentElement.removeChild(element);
+removeElement('#wrapper-footer-1');
 
 // remove footer region
-var element = document.querySelector('footer.cattagsarea');
-element.parentElement.removeChild(element);
-
-// remove paddings and margins
-var element = document.querySelector('#single-wrapper');
-element.style.paddingTop = '0px';
-element.style.paddingBottom = '0px';
+removeElement('footer.cattagsarea');
 
 // remove email subscription
-var element = document.querySelector('div.alert-banner');
-element.parentElement.removeChild(element);
+removeElement('div.alert-banner');
 
 // remove image of author at the top of the blog
-var element = document.querySelector('div.author-header');
-element.parentElement.removeChild(element);
+removeElement('div.author-header');
 
 // remove image of author at the top of the blog
-var element = document.querySelector('span.entry-meta-comment');
-element.parentElement.removeChild(element);
+removeElement('span.entry-meta-comment');
 
 // this statement is duplicated and executed for the second-time, this is not a bug
 // we need to execute this for the second time
-var element = document.querySelector('span.entry-meta-comment');
-element.parentElement.removeChild(element);
+removeElement('span.entry-meta-comment');
 
 // remove the social links at the bottom of the blog
-var element = document.querySelector('div.social-links');
-element.parentElement.removeChild(element);
+removeElement('div.social-links');
 
-var element = document.querySelector('div.entry-meta-layout');
+// remove paddings and margins
+let element = document.querySelector('#single-wrapper');
+element.style.paddingTop = '0px';
+element.style.paddingBottom = '0px';
+
+element = document.querySelector('div.entry-meta-layout');
 element.style.marginBottom = '0px';
 
-var element = document.querySelector('#featured');
+element = document.querySelector('#featured');
 element.style.paddingLeft = '0px';
 element.style.paddingRight = '0px';
 element.style.marginRight = '0px';
 element.style.marginLeft = '0px';
 element.style.paddingTop = '0px';
+
+function removeElement(elementName) {
+    try {
+        const element = document.querySelector(elementName);
+        if (element && element.parentElement)
+            element.parentElement.removeChild(element);
+    } catch (ex) {
+        console.log(`Exception when removing element "${elementName}"`);
+        console.log(`Exception : ${ex}`);
+    }
+}
