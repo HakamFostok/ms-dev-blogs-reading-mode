aa12d7dcdb86347b3024c6e2f479d387de7095d6
Hakan Fıstık <hakam.fstk@gmail.com>
2026-06-30 12:28:59 +0200

    fix

--- src/devblogsReadingMode.js
+++ src/devblogsReadingMode.js
@@ -1,13 +1,12 @@
 removeElement('div.container-evo');
 removeElement('aside.post-sidebar');
 removeElement('div.left-sidebar');
-removeElement('div#usabilla-button');
-removeElement('div#footerArea');
+removeElement('#uhf-footer');
 removeElement('div.stay-informed');
 removeElement('div#comments');
 removeElement('div.container-three-column-even');
 removeElement('div.evo-right-sidebar');
-removeElement('header.c-uhfh.context-uhf.c-sgl-stck.c-category-header');
+removeElement('header.uhf-header');
 removeElement('div.border-bottom.pb-40.mb-64');
 removeElement('div.d-flex.justify-content-between.flex-column.flex-lg-row.align-items-center.gap-24.mb-40');
 removeElement('h2.fs-20.mb-24');
@@ -15,30 +14,32 @@ removeElement('div.d-flex.gap-24.flex-column.flex-lg-row');
 
 let element = document.querySelector('div.container-evo');
 if (element)
-    element.classList.remove("mt-56");
+  element.classList.remove("mt-56");
 
 element = document.querySelector("#single-wrapper");
 if (element)
-    element.classList.remove("container-three-column-post");
+  element.classList.remove("container-three-column-post");
 
 element = document.querySelector("article.middle-column.pe-xl-198");
 if (element) {
-    element.classList.remove("middle-column");
-    element.classList.remove("pe-xl-198");
+  element.classList.remove("middle-column");
+  element.classList.remove("pe-xl-198");
 }
 
 element = document.getElementsByTagName('h2');
 for (let i = 0; i < element.length; i++) {
-     element[i].style.margin = '0';
+  element[i].style.margin = '0';
 }
 
 function removeElement(elementName) {
-    try {
-        const element = document.querySelector(elementName);
-        if (element && element.parentElement)
-            element.parentElement.removeChild(element);
-    } catch (ex) {
-        console.log(`Exception when removing element "${elementName}"`);
-        console.log(`Exception : ${ex}`);
-    }
+  try {
+    const element = document.querySelector(elementName);
+    if (element && element.parentElement)
+      element.parentElement.removeChild(element);
+    else
+      console.log(`Element "${elementName}" not found`);
+  } catch (ex) {
+    console.log(`Exception when removing element "${elementName}"`);
+    console.log(`Exception : ${ex}`);
+  }
 }
